99b0432196a999a9d45de09a259f3fa7274a3d4b
Helena Thiessen <tlena43@gmail.com>
2026-03-06 19:02:20 -0800

    added onclick functionality for story cards in voting page

--- frontend/src/Voting.jsx
+++ frontend/src/Voting.jsx
@@ -1,23 +1,44 @@
 import React, { useState, useEffect } from 'react';
 import './index.css';
 import { api } from "./global.jsx"
-import StoryCard from './StoryVoteCard.jsx';
+
+//a react component to display an individual story
+//to display for voting
+const StoryCard = ({ story, isSelected, onClick, id }) => {
+  if (!story) return null;
+
+  const classes = isSelected ? "story-vote-card selected-card" : "story-vote-card";
+
+  return (
+    <div className={classes} onClick={() => onClick(id)}>
+      <p>{story ?? "No content available"}</p>
+    </div>
+  );
+}
 
 //react component to store of list of StoryVoteCards to 
 //display the stories that are being voted on
 const StoryCardList = () => {
-  let tempStory = "this is a temp story to display until the storytelling is implemented"
-  let tempStory2 = "this is a second temp story to test voting"
-  return (
-    <div id="story-vote-card-list">
-      <StoryCard story={tempStory}></StoryCard>
-      <StoryCard story={tempStory2}></StoryCard>
-      <StoryCard story={tempStory}></StoryCard>
-      <StoryCard story={tempStory2}></StoryCard>
-    </div>
-  )
-}
+  let tempStory = "this is a temp story to display until the storytelling works";
+  let tempStory2 = "this is a second temp story to test voting";
+  let stories = [tempStory, tempStory2, tempStory, tempStory2];
 
+  const [selectedStory, setSelectedStory] = useState(null);
+
+  const handleStoryClick = (id) => {
+    setSelectedStory(id);
+  }
+
+  return (
+    <div id='story-vote-card-list'>
+      {stories.map((story, idx) => (
+        <StoryCard story={story} key={idx} id={idx} onClick={handleStoryClick} isSelected={idx === selectedStory}/>
+      ))}
+    </div>
+  );
+};
+
+//react component for the window footer
 const ControlBar = () => {
   return (
     <div className="game-window-control-bar">
@@ -26,6 +47,7 @@ const ControlBar = () => {
   )
 }
 
+//react component for the window footer
 const Header = () => {
   return (
     <div className="game-window-header">
