7a24ef8287c61f583f8674c64a0286330a69e14f
Helena Thiessen <tlena43@gmail.com>
2026-03-13 16:21:17 -0700

    added user_id cookie to store id in browser

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,21 @@
 import os
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, abort, g, jsonify, session
 from flask_restful import Resource, Api
 from flask_compress import Compress
 from itsdangerous import TimestampSigner
 from dotenv import load_dotenv
-import models  
-from models import db
-from flask import jsonify
+from models import *
+from itsdangerous import TimestampSigner
+from functools import wraps
+import uuid
+
 
 load_dotenv()
+s = TimestampSigner(os.getenv("secretKey"))
 
 def create_app(test_config: dict | None = None):
     app = Flask(__name__)
+    app.config["secretKey"] = os.getenv("secretKey")
 
     # allow tests to override config easily
     app.config.update(
@@ -29,15 +33,19 @@ def create_app(test_config: dict | None = None):
 
     @app.before_request
     def beforeRequest():
-        if app.config.get("TESTING"):
-            return
+        token = request.cookies.get("uid")
 
-        criteria = [request.is_secure, app.debug, request.headers.get("X-Forwarded-Proto", "http") == "https"]
-        if not any(criteria):
-            url = request.url.replace("http://", "https://", 1)
-            return redirect(url, code=308)
+        if token:
+            try:
+                user_id = signer.unsign(token.encode("utf8"), max_age=60*60*24*365)
+                g.user = User.get(User.user_id == int(user_id))
+                return
+            except Exception:
+                pass
 
-        db.connect(reuse_if_open=True)
+        # if no cookie create user
+        user = User.create(user_id=uuid.uuid4())
+        g.user = user
 
     @app.after_request
     def afterRequest(response):
@@ -52,22 +60,35 @@ def create_app(test_config: dict | None = None):
                     db.close()
             except Exception:
                 pass
+            
+        if not request.cookies.get("uid") and hasattr(g, "user"):
+            signed = signer.sign(str(g.user.user_id).encode("utf8")).decode("utf8")
+
+            response.set_cookie(
+                "uid",
+                signed,
+                max_age=60*60*24*365,
+                httponly=True,
+                samesite="Lax"
+            )
 
         return response
+
 
     #endpoints
     class SampleEndpoint(Resource):
         def get(self):
             items = []
-            for g in models.Game.select():
+            for game in Game.select():
                 items.append({
-                    "game_id": str(g.game_id),
-                    "game_status": str(g.game_status_id),   
-                    "game_host": str(g.game_host_id),
+                    "game_id": str(game.game_id),
+                    "game_status": str(game.game_status_id),   
+                    "game_host": str(game.game_host_id),
                 })
             return jsonify({"items": items})
 
     api.add_resource(SampleEndpoint, "/Sample")
+    
 
     return app
 
