bad4ff785bcd3093ff978d5240fb445802ea279c
Helena Thiessen <tlena43@gmail.com>
2026-03-25 12:44:31 -0700

    updated loaddb

--- backend/loaddb.py
+++ backend/loaddb.py
@@ -2,9 +2,10 @@ from models import *
 
 def reset_tables():
     db.connect(reuse_if_open=True)
-    db.drop_tables([Story_Part, Voting, Story, Voting_Session, 
-                    Game_Players, Game_Settings, Game, User, Status, Story_Assignment, Round_State], safe=True, cascade=True)
-    db.create_tables([Status, User, Game, Game_Settings, Game_Players,
+    db.drop_tables([Story_Part, Voting, Story, Voting_Session, Voting_Category, 
+                    Game_Players, Game_Settings, Game, User, Status, 
+                    Story_Assignment, Round_State], safe=True, cascade=True)
+    db.create_tables([Status, User, Game, Game_Settings, Game_Players, Voting_Category,
                       Voting_Session, Story, Voting, Story_Part, Story_Assignment, Round_State], safe=True)
     db.close()
 
